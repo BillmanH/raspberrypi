fa350e2ffa23af767d78589cd56243ee9526cfc5
Billmanh <william.jeffrey.harding@gmail.com>
2019-10-13 18:58:04 -0700

    split up tests, jacket working

--- blinka/10_single_test.py
+++ blinka/10_single_test.py
@@ -5,16 +5,19 @@ import neopixel
 
 numled = 60 
 
-pixels = neopixel.NeoPixel(board.D18, numled)
+pixels = neopixel.NeoPixel(board.D10, numled)
+
+
 
 for i in range(3):
     print(i)
     for i in range(numled):
-        print(i),
+        print(i , end=' ')
         pixels[i] = (np.random.randint(0,255),np.random.randint(0,255),np.random.randint(0,255))
 
     for i in range(numled):
         pixels[(numled-1)-i] = (0,0,0)
+
 
 
 
--- blinka/18_all_on.py
+++ blinka/18_all_on.py
@@ -2,19 +2,21 @@ import numpy as np
 
 import board
 import neopixel
-pixels = neopixel.NeoPixel(board.D18, 180)
 
+numled = 600
 
-numled = 200
+pixels = neopixel.NeoPixel(board.D18, numled)
 
-chosen = 1
-
-pixels[chosen] = (0,0,0)
 
 for i in range(3):
+    print(i)
     for i in range(numled):
-        print(i)
+        print(i,end=" ")
         pixels[i] = (np.random.randint(0,255),np.random.randint(0,255),np.random.randint(0,255))
 
     for i in range(numled):
-        pixels[(numled-1)-i] = (0,0,0) 
+        pixels[(numled-1)-i] = (0,0,0)
+
+
+
+
