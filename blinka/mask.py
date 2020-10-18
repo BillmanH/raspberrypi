300c6ce3e480b7b67e4864763413463c0c43f69e
Bill Harding <william.harding@avanade.com>
2020-10-18 13:49:04 -0700

    added try

--- blinka/mask.py
+++ blinka/mask.py
@@ -6,9 +6,11 @@ import neopixel
 import read_excel
 
 # %%
-pixels = neopixel.NeoPixel(board.D18, 165)
+pixels = neopixel.NeoPixel(board.D18, 166)
 
 # %%
 for i in read_excel.pattern:
-    print(i)
-    pixels[i[0]] = (i[1][0], i[1][1], i[1][2])
+    try:
+        pixels[i[0]] = (i[1][0], i[1][1], i[1][2])
+    except:
+        pass
