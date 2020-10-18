69447bef5a5630f0200adbe714465bbbfdcb2783
Bill Harding <william.harding@avanade.com>
2020-10-18 14:05:19 -0700

    fixed y not found bug

--- blinka/sparkle_mask.py
+++ blinka/sparkle_mask.py
@@ -9,9 +9,10 @@ import read_excel
 pixels = neopixel.NeoPixel(board.D18, 165)
 
 # %%
+y = 0
 myRange = range(300)
 for i in myRange:
-    pixels[y] = (0,0,0)
+    pixels[y] = (0, 0, 0)
     y = np.random.randint(0, 165)
     rgb = (np.random.randint(0, 255),
            np.random.randint(0, 255),
